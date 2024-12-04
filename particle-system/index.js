40c20cd94dd1e328e08867efbc772ea591b5d3f1
Fabian Morón Zirfas <hash@ff6347.email>
2024-12-04 10:26:02 +0100

    feat: The xmas particle tree

--- particle-system/index.js
+++ particle-system/index.js
@@ -12,7 +12,8 @@ class Particle {
 	y = 0;
 	lifetime;
 	isDead = false;
-	constructor(_x, _y, _lifetime = 100, _isRandom = false) {
+	grey = 0;
+	constructor({ _x, _y, _lifetime = 100, _isRandom = false }) {
 		this.x = _x;
 		this.y = _y;
 		this.lifetime = _lifetime;
@@ -23,12 +24,13 @@ class Particle {
 	display() {
 		push();
 		noFill();
+		stroke(map(this.lifetime, 0, 100, 100, 255));
 		translate(this.x, this.y);
 		rotate(this.angle);
-		circle(0, 0, 10);
+		// circle(0, 0, 10);
 		point(0, 0);
-		line(-5, -5, 5, 5);
-		line(-5, 5, 5, -5);
+		// line(-5, -5, 5, 5);
+		// line(-5, 5, 5, -5);
 		pop();
 	}
 	update() {
@@ -39,8 +41,8 @@ class Particle {
 			this.x = this.x + random(-1, 1);
 			this.y = this.y + random(-1, 1);
 		} else {
-			this.x += noise(this.xOff) * this.speed - this.speed / 2;
-			this.y += noise(this.yOff) * this.speed - this.speed / 2;
+			this.x += noise(this.xOff) * (this.speed * 2) - (this.speed * 2) / 2;
+			this.y += (noise(this.yOff) * this.speed) / 2 + cos(this.angle / 6);
 			this.xOff += 0.01;
 			this.yOff += 0.01;
 		}
@@ -53,24 +55,40 @@ class Particle {
 }
 
 const particles = [];
+let allDead = false;
 
 function setup() {
+	console.log(easeOutExpo);
 	const canvas = createCanvas(550, 550);
 	canvas.parent("sketch");
 	background("white");
-	for (y = height; y >= 10; y--) {
-		particles.push(
-			new Particle(width / 2, y, random(50, 200), random() > 0.5 ? false : true)
-		);
+	for (let y = height - 150; y >= 60; y--) {
+		for (let i = 0; i < 10; i++) {
+			particles.push(
+				new Particle({
+					_x: width / 2,
+					_y: y,
+					_lifetime: random(50, 200),
+					_isRandom:
+						random() > easeOutQuint(y, 0, 1, height - 250) ? true : false,
+				})
+			);
+		}
 	}
+	//saveGif("output.gif", 5, { delay: 0.2 });
 }
 
 function draw() {
-	background(255, 50);
+	background(255, 1);
 	for (let i = 0; i < particles.length; i++) {
 		const p = particles[i];
 		p.display();
 		p.update();
+	}
+
+	allDead = particles.every((p) => p.isDead === true);
+	if (allDead) {
+		noLoop();
 	}
 
 	// for (let j = particles.length - 1; j >= 0; j--) {
@@ -78,9 +96,4 @@ function draw() {
 	// 		particles.splice(j, 1);
 	// 	}
 	// }
-}
-
-function mouseDragged() {
-	const oneParticle = new Particle(mouseX, mouseY, 100, false);
-	particles.push(oneParticle);
 }
